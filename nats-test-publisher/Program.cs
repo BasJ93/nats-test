0b7e22ec33e1dfc0463a3c3aab7573da68473a47
Bas Janssen <smh.janssen@gmail.com>
2023-04-11 07:44:48 +0200

    Initial commit with working publisher and subscriber.

---
+++ nats-test-publisher/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using NATS.Client;
+using NATS.Client.JetStream;
+
+string subject = "test-subject";
+string deliverySubject = "test-delivery-subject";
+string stream = "test-stream";
+
+Console.WriteLine("NATS test publisher");
+
+try
+{
+    using IConnection c = new ConnectionFactory().CreateConnection("localhost");
+    IJetStreamManagement jetStreamManagement = c.CreateJetStreamManagementContext();
+
+    StreamConfiguration streamConfiguration = StreamConfiguration.Builder()
+        .WithName(stream)
+        .WithStorageType(StorageType.Memory)
+        .WithRetentionPolicy(RetentionPolicy.WorkQueue)
+        .WithSubjects(new []{subject})
+        .Build();
+
+    StreamInfo streamInfo = jetStreamManagement.AddStream(streamConfiguration);
+    Console.WriteLine("Created stream {0} with subject {1}", stream, subject);
+
+    // create a JetStream context
+    IJetStream js = c.CreateJetStreamContext();
+        
+    for (int i = 0; i < 1000; i++)
+    {
+        byte[] data = Encoding.UTF8.GetBytes("Test message " + i);
+
+        Msg msg = new Msg(subject, data);
+
+        PublishAck pa = js.Publish(msg);
+            
+        Console.WriteLine("Published message '{0}' on subject '{1}', stream '{2}', seqno '{3}'.",
+            Encoding.UTF8.GetString(data), deliverySubject, pa.Stream, pa.Seq);
+    }
+
+    //Console.ReadKey();
+
+    //jetStreamManagement.DeleteStream(stream);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex);
+}
---
+++ nats-test-subscriber/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using NATS.Client;
+using NATS.Client.JetStream;
+
+string subject = "test-subject";
+string durable = "required-durable";
+
+Console.WriteLine("NATS test subscriber");
+Console.WriteLine("Running in pull mode");
+
+// TODO: Change to pull mode
+
+try
+{
+
+    Options options = ConnectionFactory.GetDefaultOptions();
+
+    options.Url = "nats://localhost:4222";
+    options.AllowReconnect = true;
+    options.MaxReconnect = Options.ReconnectForever;
+    options.PingInterval = 30000;
+    
+
+    options.ReconnectedEventHandler = (obj, args) =>
+    {
+        Console.WriteLine($"Reconnected to {args.Conn.ConnectedUrl}. New status: {args.Conn.State.ToString()}");
+    };
+    options.DisconnectedEventHandler = (obj, args) =>
+    {
+        Console.WriteLine($"Connection was disconnected. New status: {args.Conn.State.ToString()}");
+    };
+    options.ClosedEventHandler = (obj, args) =>
+    {
+        Console.WriteLine($"Connection was closed. New status: {args.Conn.State.ToString()}");
+    };
+    
+    using IConnection c = new ConnectionFactory().CreateConnection(options);
+    IJetStreamManagement jetStreamManagement = c.CreateJetStreamManagementContext();
+
+
+    // create a JetStream context
+    IJetStream js = c.CreateJetStreamContext();
+
+    // For this, durable seems to be the queue group
+    PullSubscribeOptions pullOptions = PullSubscribeOptions.Builder()
+        .WithDurable(durable)
+        .Build();
+
+    IJetStreamPullSubscription sub = js.PullSubscribe(subject, pullOptions);
+    
+    c.Flush(500);
+
+    while (true)
+    {
+        sub.Pull(1);
+        try
+        {
+            //Msg? msg = sub.NextMessage(500);
+            Msg? msg = sub.NextMessage(500);
+            if (msg != null)
+            {
+                Console.WriteLine(Encoding.UTF8.GetString(msg.Data) + " id: " + msg.MetaData.StreamSequence);
+                Task.Delay(200);
+                msg.Ack();
+            }
+        }
+        catch (NATSTimeoutException)
+        {
+            // timeout is acceptable, means no messages available.
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
